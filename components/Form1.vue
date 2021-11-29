3d4ab788649eb7d2e6c920fa1803cbe093b7a5af
sanchitagithub12 <sanchitagithub@gmail.com>
2021-11-29 17:30:56 +0530

    completed

--- components/Form1.vue
+++ components/Form1.vue
@@ -4,11 +4,22 @@
       <div class="col-md-4"></div>
       <div class="col-md-3"><h1 align="center">Customer</h1></div>
     </div>
-    <v-snackbar v-model="snackbar" absolute top right color="success">
+    <v-snackbar
+      v-model="snackbar"
+      absolute
+      top
+      right
+      class="snack"
+      color="#64B5F6"
+    >
       <span>Submit successful!</span>
       <v-icon dark> mdi-checkbox-marked-circle </v-icon>
     </v-snackbar>
-    <form ref="form" @submit.prevent="saveCustomer">
+    <form
+      ref="form"
+      @submit.prevent="saveCustomer"
+      v-if="$vuetify.breakpoint.mdAndUp"
+    >
       <div class="row">
         <div class="col-md-1"></div>
         <div class="col-md-3">
@@ -187,7 +198,195 @@
               class="btn btn-success"
               :disabled="!formIsValid"
               text
-              color="success"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+    <form
+      ref="form"
+      @submit.prevent="saveCustomer"
+      v-if="$vuetify.breakpoint.smAndDown"
+    >
+      <div class="row">
+        <!-- <div class="col-md-1"></div> -->
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>First Name:</label>
+            <input
+              v-model="form.first"
+              v-on:keypress="isLetter($event)"
+              :rules="rules.name"
+              color="blue"
+              label="First Name"
+              required
+              class="form-control"
+              name="name"
+            />
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Middle Name:</label>
+            <input
+              v-model="form.middle"
+              v-on:keypress="isLetter($event)"
+              :rules="rules.name"
+              color="blue"
+              label="Middle Name"
+              required
+              class="form-control"
+            />
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Sur Name:</label>
+            <input
+              v-model="form.sur"
+              v-on:keypress="isLetter($event)"
+              :rules="rules.name"
+              color="blue"
+              label="Sur Name"
+              required
+              class="form-control"
+            />
+          </div>
+        </div>
+
+        <!-- <div class="col-md-1"></div> -->
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Age:</label>
+            <input
+              type="text"
+              @keypress="onlyNumber"
+              v-model="form.age"
+              :rules="rules.phone"
+              color="blue"
+              label="Age"
+              maxlength="2"
+              class="form-control"
+              required
+            />
+          </div>
+        </div>
+        <!-- <div class="col-md-1"></div> -->
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Mobile:</label>
+            <input
+              type="text"
+              @keypress="onlyNumber"
+              v-model="form.mobile"
+              :rules="rules.phone"
+              color="blue"
+              label="Phone Number"
+              maxlength="10"
+              class="form-control"
+              required
+            />
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Gender:</label>
+            <!-- <input
+              v-model="form.gender"
+              v-on:keypress="isLetter($event)"
+              :rules="rules.name"
+              color="blue"
+              label="Gender"
+              required
+              class="form-control"
+            /> -->
+            <select
+              class="form-control"
+              id="sel1"
+              v-model="form.gender"
+              color="blue"
+              label="Gender"
+              required
+            >
+              <option>male</option>
+              <option>female</option>
+              <option>other</option>
+            </select>
+          </div>
+        </div>
+
+        <!-- <div class="col-md-1"></div> -->
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Village:</label>
+            <input
+              v-model="form.village"
+              v-on:keypress="isLetter($event)"
+              :rules="rules.name"
+              color="blue"
+              label="Village"
+              required
+              class="form-control"
+            />
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Address:</label>
+            <input
+              v-model="form.address"
+              :rules="rules.name"
+              color="blue"
+              label="address"
+              required
+              class="form-control"
+            />
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Blood Group:</label>
+            <!-- <input
+              v-model="form.bloodGroup"
+              :rules="rules.name"
+              color="blue"
+              label="Blood Group"
+              required
+              class="form-control"
+            /> -->
+            <select
+              class="form-control"
+              id="sel2"
+              v-model="form.bloodGroup"
+              color="blue"
+              label="Blood Group"
+              required
+            >
+              <option>A+</option>
+              <option>B+</option>
+              <option>A-</option>
+              <option>B-</option>
+              <option>O+</option>
+              <option>O-</option>
+              <option>AB+</option>
+              <option>AB-</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <div class="row">
+        <div class="col-md-5"></div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <button
+              class="btn btn-success"
+              :disabled="!formIsValid"
+              text
               type="submit"
             >
               Submit
@@ -231,14 +430,12 @@
         <div class="col-md-5"></div>
         <div class="col-md-4">
           <div class="form-group">
-            <button class="btn btn-success" text color="success" type="submit">
-              Search
-            </button>
+            <button class="btn btn-success" text type="submit">Search</button>
           </div>
         </div>
       </div>
     </form>
-    <table class="table table-hover">
+    <!-- <table class="table table-hover" style="background-color: skyblue">
       <thead>
         <tr>
           <th>First Name</th>
@@ -255,11 +452,7 @@
 
       <tbody>
         <tr>
-          <!-- <tr v-for="post in posts.Info.slice(
-            posts.Info.length - 1,
-            posts.Info.length
-          )"
-          :key="post._id"></tr> -->
+         
           <td>{{ this.customers.firstName }}</td>
           <td>{{ this.customers.middleName }}</td>
           <td>{{ this.customers.surName }}</td>
@@ -270,12 +463,66 @@
           <td>{{ this.customers.address }}</td>
           <td>{{ this.customers.bloodGroup }}</td>
         </tr>
-        <!-- 
-        {{
-          this.customers.firstName
-        }} -->
+        
       </tbody>
-    </table>
+    </table> -->
+    <!-- <table class="table table-sm table-dark">
+      <thead>
+        <tr>
+          <th scope="col">First Name</th>
+          <th scope="col">Middle Name</th>
+          <th scope="col">Sur Name</th>
+          <th scope="col">Age</th>
+          <th scope="col">Mobile</th>
+          <th scope="col">Gender</th>
+          <th scope="col">Village</th>
+          <th scope="col">Address</th>
+          <th scope="col">Blood Group</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row"></th>
+          <td>{{ this.customers.firstName }}</td>
+          <td>{{ this.customers.middleName }}</td>
+          <td>{{ this.customers.surName }}</td>
+          <td>{{ this.customers.age }}</td>
+          <td>{{ this.customers.mobile }}</td>
+          <td>{{ this.customers.gender }}</td>
+          <td>{{ this.customers.village }}</td>
+          <td>{{ this.customers.address }}</td>
+          <td>{{ this.customers.bloodGroup }}</td>
+        </tr>
+      </tbody>
+    </table> -->
+    <div class="container" style="background-color: skyblue" align="center">
+      <div class="row">
+        <div class="col-md-4">
+          <h4>
+            Full Name: <span></span> {{ this.customers.firstName }}
+            {{ this.customers.middleName }} {{ this.customers.surName }}
+          </h4>
+        </div>
+        <div class="col-md-4">
+          <h4>Age: <span></span> {{ this.customers.age }}</h4>
+        </div>
+        <div class="col-md-4">
+          <h4>Mobile: <span></span> {{ this.customers.mobile }}</h4>
+        </div>
+        <div class="col-md-4">
+          <h4>Gender: <span></span> {{ this.customers.gender }}</h4>
+        </div>
+        <div class="col-md-4">
+          <h4>Village: <span></span> {{ this.customers.village }}</h4>
+        </div>
+        <div class="col-md-4">
+          <h4>Address: <span></span> {{ this.customers.address }}</h4>
+        </div>
+        <div class="col-md-4">
+          <h4>Blood Group: <span></span> {{ this.customers.bloodGroup }}</h4>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -415,3 +662,14 @@ export default {
   },
 }
 </script>
+<style >
+.btn-success {
+  background-color: #64b5f6 !important;
+  /* border: #64b5f6 solid 10px; */
+  outline-color: #64b5f6 !important;
+}
+.cantainer {
+  display: grid;
+  place-content: center;
+}
+</style>
