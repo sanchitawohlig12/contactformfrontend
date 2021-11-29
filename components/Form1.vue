cc8533fa75cb88e320c35f33941b32ff47b487a9
sanchitagithub12 <sanchitagithub@gmail.com>
2021-11-29 15:30:44 +0530

    completed

--- components/Form1.vue
+++ components/Form1.vue
@@ -22,6 +22,7 @@
               label="First Name"
               required
               class="form-control"
+              name="name"
             />
           </div>
         </div>
@@ -101,15 +102,18 @@
               required
               class="form-control"
             /> -->
-            <select class="form-control" id="sel1" v-model="form.gender"
-              
+            <select
+              class="form-control"
+              id="sel1"
+              v-model="form.gender"
               color="blue"
               label="Gender"
-              required>
-    <option>Male</option>
-    <option>Female</option>
-    
-  </select>
+              required
+            >
+              <option>male</option>
+              <option>female</option>
+              <option>other</option>
+            </select>
           </div>
         </div>
       </div>
@@ -145,14 +149,31 @@
         <div class="col-md-3">
           <div class="form-group">
             <label>Blood Group:</label>
-            <input
+            <!-- <input
               v-model="form.bloodGroup"
               :rules="rules.name"
               color="blue"
               label="Blood Group"
               required
               class="form-control"
-            />
+            /> -->
+            <select
+              class="form-control"
+              id="sel2"
+              v-model="form.bloodGroup"
+              color="blue"
+              label="Blood Group"
+              required
+            >
+              <option>A+</option>
+              <option>B+</option>
+              <option>A-</option>
+              <option>B-</option>
+              <option>O+</option>
+              <option>O-</option>
+              <option>AB+</option>
+              <option>AB-</option>
+            </select>
           </div>
         </div>
       </div>
@@ -175,44 +196,48 @@
         </div>
       </div>
     </form>
-    <div class="row">
-      <div class="col-md-2"></div>
-      <div class="col-md-3">
-        <div class="form-group">
-          <label>First Name:</label>
-          <input v-model="form.first"
+    <form ref="form1" @submit.prevent="searchfirstName">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>First Name:</label>
+            <input
+              v-model="form.firstName"
               v-on:keypress="isLetter($event)"
               :rules="rules.name"
               color="blue"
               label="First Name"
               required
-              class="form-control" />
+              class="form-control"
+            />
+          </div>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-3">
+          <div class="form-group">
+            <label>Register key</label>
+            <input
+              v-model="form.key"
+              color="blue"
+              label="key"
+              required
+              class="form-control"
+            />
+          </div>
         </div>
       </div>
-      <div class="col-md-1"></div>
-      <div class="col-md-3">
-        <div class="form-group">
-          <label>Register key</label>
-          <input color="blue" required class="form-control" />
+      <div class="row">
+        <div class="col-md-5"></div>
+        <div class="col-md-4">
+          <div class="form-group">
+            <button class="btn btn-success" text color="success" type="submit">
+              Search
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-md-5"></div>
-      <div class="col-md-4">
-        <div class="form-group">
-          <button
-            class="btn btn-success"
-            text
-            color="success"
-            type="submit"
-            @click="searchfirstName"
-          >
-            Search
-          </button>
-        </div>
-      </div>
-    </div>
+    </form>
     <table class="table table-hover">
       <thead>
         <tr>
@@ -229,22 +254,26 @@
       </thead>
 
       <tbody>
-        <tr v-for="cust in customers" :key="cust._id">
+        <tr>
           <!-- <tr v-for="post in posts.Info.slice(
             posts.Info.length - 1,
             posts.Info.length
           )"
           :key="post._id"></tr> -->
-          <td>{{ cust.firstName }}</td>
-          <td>{{ cust.middleName }}</td>
-          <td>{{ cust.surName }}</td>
-          <td>{{ cust.age }}</td>
-          <td>{{ cust.mobile }}</td>
-          <td>{{ cust.gender }}</td>
-          <td>{{ cust.village }}</td>
-          <td>{{ cust.address }}</td>
-          <td>{{ cust.bloodGroup }}</td>
+          <td>{{ this.customers.firstName }}</td>
+          <td>{{ this.customers.middleName }}</td>
+          <td>{{ this.customers.surName }}</td>
+          <td>{{ this.customers.age }}</td>
+          <td>{{ this.customers.mobile }}</td>
+          <td>{{ this.customers.gender }}</td>
+          <td>{{ this.customers.village }}</td>
+          <td>{{ this.customers.address }}</td>
+          <td>{{ this.customers.bloodGroup }}</td>
         </tr>
+        <!-- 
+        {{
+          this.customers.firstName
+        }} -->
       </tbody>
     </table>
   </div>
@@ -265,10 +294,13 @@ export default {
       village: '',
       address: '',
       bloodGroup: '',
+      key: '',
+      firstName: '',
     })
 
     return {
-      customers: null,
+      customers: '',
+
       form: Object.assign({}, defaultForm),
       rules: {
         name: [(val) => (val || '').length > 0 || 'This field is required'],
@@ -363,11 +395,18 @@ export default {
     //     });
     // }
     searchfirstName() {
-      CustomerService.findByfirstName(this.firstName && this.key)
-        .then((response) => {
-          this.customers = response.data
+      const data = {
+        firstName: this.form.firstName,
+        key: this.form.key,
+      }
+      CustomerService.findByfirstName(data)
 
-          console.log(response.data)
+        .then((response) => {
+          // console.log(response.data)
+          this.customers = response.data
+          console.log(this.customers)
+
+          // console.log(response.data)
         })
         .catch((e) => {
           console.log(e)
